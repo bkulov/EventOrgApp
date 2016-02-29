197b5ab3b06e659ea0e6c2680f11e33afed8d8c7
Borislav Kulov <bkulov@gmail.com>
2016-02-29 10:19:59 +0200

    Use google apis to request data

--- source/services/google.service.js
+++ source/services/google.service.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './utils.service'], function(exports_1) {
+System.register(['angular2/core', 'angular2/router', './utils.service'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,22 +8,29 @@ System.register(['angular2/core', './utils.service'], function(exports_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, utils_service_1;
+    var core_1, router_1, utils_service_1;
     var GoogleService;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
             function (utils_service_1_1) {
                 utils_service_1 = utils_service_1_1;
             }],
         execute: function() {
             GoogleService = (function () {
-                function GoogleService(_utilsService) {
+                function GoogleService(_utilsService, _router) {
                     this._utilsService = _utilsService;
+                    this._router = _router;
+                    // TODO: check if this is needed
+                    //gapi.auth.init(() => {});
                 }
                 // https://developers.google.com/oauthplayground
+                // https://developers.google.com/apis-explorer/#p/
                 // 1 - select and authorize APIs
                 // 2 - get authorization token
                 // 3 - GET request for a given resource. Authorization token from step 2 will be added to Authorization header
@@ -35,34 +42,61 @@ System.register(['angular2/core', './utils.service'], function(exports_1) {
                         'https://www.google.com/m8/feeds/contacts/default/full/'
                     ];
                     var config = {
-                        'client_id': this._config.web.client_id,
-                        'scope': scopes
+                        'client_id': this._clientConfig.web.client_id,
+                        'scope': scopes,
+                        immediate: false // use true to refresh to token without showing UI
                     };
-                    gapi.auth.authorize(config, function () {
+                    gapi.auth.authorize(config, function (token) {
                         console.log('login complete');
-                        _this._token = gapi.auth.getToken();
-                        console.log(_this._token);
+                        console.log(token);
+                        _this._token = token;
                         // TODO: redirect to another page
+                        //this._router.navigate(['Agenda']);
                     });
                 };
                 GoogleService.prototype._getContacts = function () {
-                    // TODO: https GET request
+                    // google plus using load
+                    gapi.client.load('plus', 'v1')
+                        .then(function (context) {
+                        gapi.client.plus.activities.search({ 'query': 'Google+', 'orderBy': 'best' }).then(function (response) {
+                            console.log('loading Google+ feed:');
+                            console.log(response.result);
+                        }, function (reason) {
+                            console.log('Error: ' + reason.result.error.message);
+                        });
+                    });
+                    // contacts
+                    gapi.client.request({
+                        'path': 'm8/feeds/contacts/default/full'
+                    })
+                        .then(function (response) {
+                        console.log('loading Contacts:');
+                        console.log(response);
+                        window.open("data:text/xml;charset=utf-8," + response.body);
+                    }, function (error) {
+                        console.log(error);
+                    });
                 };
                 GoogleService.prototype.login = function () {
                     var _this = this;
-                    var aa = this._utilsService.loadJSON('/google_client_id.json', (function (content) {
-                        _this._config = JSON.parse(content);
-                        if (!_this._token) {
+                    if (!this._clientConfig) {
+                        this._utilsService.loadJSON('/google_client_id.json', (function (content) {
+                            _this._clientConfig = JSON.parse(content);
                             _this._authenticate();
+                        }));
+                    }
+                    else {
+                        if (!this._token) {
+                            this._authenticate();
                         }
                         else {
-                            _this._getContacts();
+                            this._getContacts();
                         }
-                    }));
+                    }
                 };
                 GoogleService = __decorate([
                     core_1.Injectable(), 
-                    __metadata('design:paramtypes', [utils_service_1.UtilsService])
+                    __metadata('design:paramtypes', [utils_service_1.UtilsService, router_1.Router])
                 ], GoogleService);
                 return GoogleService;
             })();
--- source/services/google.service.ts
+++ source/services/google.service.ts
@@ -1,14 +1,20 @@
 ﻿import {Injectable} from 'angular2/core';
+import {Router} from 'angular2/router';
+
 import {UtilsService} from './utils.service';
 
 @Injectable()
 export class GoogleService {
-	private _config: Object;
+	private _clientConfig: Object;
 	private _token: Object;
 
-	constructor(private _utilsService: UtilsService) { }
+	constructor(private _utilsService: UtilsService, private _router: Router) {
+		// TODO: check if this is needed
+		//gapi.auth.init(() => {});
+	}
 
 	// https://developers.google.com/oauthplayground
+	// https://developers.google.com/apis-explorer/#p/
 	// 1 - select and authorize APIs
 	// 2 - get authorization token
 	// 3 - GET request for a given resource. Authorization token from step 2 will be added to Authorization header
@@ -22,32 +28,61 @@ export class GoogleService {
 		];
 
 		var config = {
-			'client_id': this._config.web.client_id,
-			'scope': scopes
+			'client_id': this._clientConfig.web.client_id,
+			'scope': scopes,
+			immediate: false	// use true to refresh to token without showing UI
         };
-        gapi.auth.authorize(config, () => {
+
+        gapi.auth.authorize(config, (token) => {
 			console.log('login complete');
-			this._token = gapi.auth.getToken();
-			console.log(this._token);
+			console.log(token);
+			this._token = token;
 
 			// TODO: redirect to another page
-
+			//this._router.navigate(['Agenda']);
         });
 	}
 
 	_getContacts(): void {
-		// TODO: https GET request
+		// google plus using load
+		gapi.client.load('plus', 'v1')
+			.then((context) => {
+				gapi.client.plus.activities.search({ 'query': 'Google+', 'orderBy': 'best' }).then(function (response) {
+					console.log('loading Google+ feed:');
+					console.log(response.result);
+				}, function (reason) {
+					console.log('Error: ' + reason.result.error.message);
+				});
+			});
+
+		// contacts
+		gapi.client.request({
+			'path': 'm8/feeds/contacts/default/full'
+		})
+			.then((response) => {
+				console.log('loading Contacts:');
+				console.log(response);
+
+				window.open("data:text/xml;charset=utf-8," + response.body);
+			}, (error) => {
+				console.log(error);
+			});
 	}
 
 	login() {
-		var aa = this._utilsService.loadJSON('/google_client_id.json', ((content: string) => {
-			this._config = JSON.parse(content);
+		if (!this._clientConfig) {
+			this._utilsService.loadJSON('/google_client_id.json', ((content: string) => {
+				this._clientConfig = JSON.parse(content);
 
+				this._authenticate();
+			}));
+		} else {
 			if (!this._token) {
 				this._authenticate();
 			} else {
 				this._getContacts();
 			}
-		}));
+		}
+
 	}
 }
